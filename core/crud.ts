49f861a15ad0bd3b593a89b55d3a590722b8157c
felipemarinhodev <felipmarinho@gmail.com>
2023-05-06 23:46:16 -0700

    utilizando typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,13 +1,13 @@
-const fs = require('fs');
+import fs from'fs';
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
   // salvar o content no sistema
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
 }
 
 // [SIMULATION]
-console.log(create("Hoje eu preciso gravar aula 2.1"));
+console.log(create("Hoje eu preciso gravar aula 2.1-extra"));
