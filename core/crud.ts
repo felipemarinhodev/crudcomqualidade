e95365edf2538f506173155832e72a8a096a58c3
felipemarinhodev <felipmarinho@gmail.com>
2023-05-07 14:47:37 -0700

    obtendo dados do arquivo e adicionando novos valores

--- core/crud.ts
+++ core/crud.ts
@@ -3,11 +3,46 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+interface Todo {
+  content: string;
+  date: string;
+  done: boolean
+}
+
 function create(content: string) {
+  const todo: Todo = {
+    content,
+    date: new Date().toISOString(),
+    done: false
+  }
+
+  const todos: Array<Todo> = [
+    ...read(),
+    todo
+  ]
+
   // salvar o content no sistema
-  fs.writeFileSync(DB_FILE_PATH, content);
-  return content;
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos
+  }, null, 2));
+  return todo;
+}
+
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}")
+  if (!db.todos) {
+    return [];
+  }
+  return db.todos;
+}
+
+function clearDB () {
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 // [SIMULATION]
-console.log(create("Hoje eu preciso gravar aula 2.1-extra"));
+clearDB()
+create("Primeiro TODO")
+create("Segunda TODO")
+console.log(read());
